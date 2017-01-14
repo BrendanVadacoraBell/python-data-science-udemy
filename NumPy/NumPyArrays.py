5eb00f974a70aa254667d0f9bf5f4ca540e77b65
Brendan Bell <brendan.vadacora.bell@gmail.com>
2017-01-14 13:17:33 +0200

    Implement basics of NumPy

---
+++ NumPy/NumPyArrays.py
@@ -0,0 +1,31 @@
+import numpy
+class NumPyArrays:
+    numpyArr = numpy.array([1, 2, 3])
+
+    print(numpyArr)
+
+    numpyRange = numpy.arange(0, 10, 2)
+
+    print(numpyRange)
+
+    numpyZeros = numpy.zeros((2,3))
+
+    print(numpyZeros)
+
+    """Evenly spaced points"""
+
+    numpySpace = numpy.linspace(0, 5, 10)
+
+    print(numpySpace)
+
+    """identity matrix"""
+
+    idMatrix = numpy.eye(4)
+
+    print(idMatrix)
+
+    """random"""
+
+    numpyRandom = numpy.random.randn(4,4)
+
+    print(numpyRandom)
